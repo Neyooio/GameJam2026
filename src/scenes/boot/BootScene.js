60fa632fa5a87aeec5827eb7a6af2f90a11de30e
Neyo <neyo@example.com>
2026-04-21 13:28:45 +0800

    Gameplay Update

--- src/scenes/boot/BootScene.js
+++ src/scenes/boot/BootScene.js
@@ -6,9 +6,16 @@ export class BootScene extends Phaser.Scene {
   preload() {
     this.load.image("bgMachine", "public/assets/images/backgrounds/Machine.jpg");
     this.load.image("bgHuman", "public/assets/images/backgrounds/Human.jpg");
+    this.load.image("propWater", "public/assets/images/props/waterbottle.png");
+    this.load.image("propCola", "public/assets/images/props/cola.png");
+    this.load.image("propCoffee", "public/assets/images/props/coffee.png");
+    this.load.image("propIce", "public/assets/images/props/icecube.png");
+    this.load.image("propTea", "public/assets/images/props/tea.png");
+    this.load.image("propJuice", "public/assets/images/props/juice.png");
     this.load.audio("menuBgm", "public/assets/audio/music/After_The_Last_Train.mp3");
     this.load.audio("rainLineSfx", "public/assets/audio/music/Rain_Against_the_Glass.mp3");
     this.load.audio("typingSfx", "public/assets/audio/sfx/typing.mp3");
+    this.load.audio("freshenUpSfx", "public/assets/audio/sfx/FreshenUp.mp3");
     this.load.audio("uiHoverSfx", "public/assets/audio/sfx/Buttonsound.mp3");
     this.load.audio("cicadaSfx", "public/assets/audio/sfx/Cicada.mp3");
     this.load.audio("staticSfx", "public/assets/audio/sfx/Static.mp3");
